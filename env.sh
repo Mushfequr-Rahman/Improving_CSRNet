5b0065e27fe1861d214a5083d958ee4498730811
Mushfequr-Rahman <rahmanmushfeq@gmail.com>
2020-02-17 02:49:13 -0500

    Added enviornment bash script

---
+++ env.sh
@@ -0,0 +1,11 @@
+conda create -n CSRNet python=3.6
+conda activate CSRNet
+conda install numpy
+conda install -c conda-forge opencv
+conda install pytorch=1.2.0
+conda install torchvision
+conda install pillow=6.1
+conda install -c conda-forge tensorboardx
+conda install tqdm
+conda install matplotlib
+
