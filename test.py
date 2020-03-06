78987cfc236c064f1eba8503c086f636f66a9b63
Mushfequr-Rahman <rahmanmushfeq@gmail.com>
2020-03-05 23:21:07 -0500

    Fixed test.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import torch
 import matplotlib.pyplot as plt
 import matplotlib.cm as CM
 from tqdm import tqdm
+from config import Config
 
-from csrnet import CSRNet
-from my_dataset import CrowdDataset
+from model import CSRNet
+from dataset import *
 
 
 def cal_mae(img_root,gt_dmap_root,model_param_path):
@@ -14,18 +15,31 @@ def cal_mae(img_root,gt_dmap_root,model_param_path):
     gt_dmap_root: the root of test ground truth density-map data.
     model_param_path: the path of specific mcnn parameters.
     '''
+    cfg= Config()
     device=torch.device("cuda")
     model=CSRNet()
     model.load_state_dict(torch.load(model_param_path))
     model.to(device)
-    dataset=CrowdDataset(img_root,gt_dmap_root,8)
-    dataloader=torch.utils.data.DataLoader(dataset,batch_size=1,shuffle=False)
+    """
+    @Mushy 
+    Changed data loader to give path From config device 
+    
+    """
+
+    dataloader = create_test_dataloader(cfg.dataset_root)
+    #dataloader=torch.utils.data.DataLoader(dataset,batch_size=1,shuffle=False)
     model.eval()
     mae=0
     with torch.no_grad():
-        for i,(img,gt_dmap) in enumerate(tqdm(dataloader)):
-            img=img.to(device)
-            gt_dmap=gt_dmap.to(device)
+        for i,data in enumerate(tqdm(dataloader)):
+            """
+            @Mushy 
+            Changed how to access the data . 
+            """
+
+            img= data['image'].to(cfg.device)
+            #gt_dmap=gt_dmap.to(device)
+            gt_dmap = data['densitymap'].to(cfg.device)
             # forward propagation
             et_dmap=model(img)
             mae+=abs(et_dmap.data.sum()-gt_dmap.data.sum()).item()
@@ -35,6 +49,9 @@ def cal_mae(img_root,gt_dmap_root,model_param_path):
 
 def estimate_density_map(img_root,gt_dmap_root,model_param_path,index):
     '''
+
+    @Mushy Hi Jess I made similar changes jere
+    You the best.
     Show one estimated density-map.
     img_root: the root of test image data.
     gt_dmap_root: the root of test ground truth density-map data.
@@ -44,25 +61,27 @@ def estimate_density_map(img_root,gt_dmap_root,model_param_path,index):
     device=torch.device("cuda")
     model=CSRNet().to(device)
     model.load_state_dict(torch.load(model_param_path))
-    dataset=CrowdDataset(img_root,gt_dmap_root,8)
-    dataloader=torch.utils.data.DataLoader(dataset,batch_size=1,shuffle=False)
+    cfg = Config()
+    dataloader = create_test_dataloader(cfg.dataset_root)
     model.eval()
-    for i,(img,gt_dmap) in enumerate(dataloader):
+    for i,data in enumerate(dataloader):
         if i==index:
-            img=img.to(device)
-            gt_dmap=gt_dmap.to(device)
+            img = data['image'].to(cfg.device)
+            # gt_dmap=gt_dmap.to(device)
+            gt_dmap = data['densitymap'].to(cfg.device)
             # forward propagation
             et_dmap=model(img).detach()
             et_dmap=et_dmap.squeeze(0).squeeze(0).cpu().numpy()
             print(et_dmap.shape)
-            plt.imshow(et_dmap,cmap=CM.jet)
+            plt.imshow(et_dmap,cmap=CM.gray)
+            plt.show()
             break
 
 
 if __name__=="__main__":
     torch.backends.cudnn.enabled=False
-    img_root='./data/Shanghai_part_A/test_data/images'
-    gt_dmap_root='./data/Shanghai_part_A/test_data/ground_truth'
-    model_param_path='./checkpoints/epoch_124.pth'
-    cal_mae(img_root,gt_dmap_root,model_param_path)
-    # estimate_density_map(img_root,gt_dmap_root,model_param_path,3) 
+    img_root='./data/part_A_final/test_data/images'
+    gt_dmap_root='./data/part_A_final/test_data/ground_truth'
+    model_param_path='./checkpoints/35.pth'
+    #cal_mae(img_root,gt_dmap_root,model_param_path)
+    estimate_density_map(img_root,gt_dmap_root,model_param_path,3)
